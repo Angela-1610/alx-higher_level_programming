a56374819f0d6c5aa302d060d3d654bb54fd85f5
Angela-1610 <angelamaged2@gmail.com>
2023-10-05 12:42:17 -0700

    files imported

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,9 @@
 if __name__ == "__main__":
     """task 3."""
     import sys
+
     args = len(sys.argv) - 1
     sum = 0
     for i in range(args):
-        sum = sum + int(sys.argv[i + 1])
+        sum += int(sys.argv[i + 1])
     print("{}".format(sum))
