3ee23ddec013ff1e815116ed720d441496039310
Angela-1610 <angelamaged2@gmail.com>
2023-11-07 14:12:30 -0800

    done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,16 +2,7 @@
 """Defines a file-writing function."""
 
 def write_file(filename="", text=""):
-    """write in file and return no of char
-
-    Args:
-        filename: file to write in
-        text: to write it in file
-
-    Returns:
-        no of char written
-    """
+    """write in file and return no of char"""
 
     with open(filename, 'w', encoding='utf=8') as x:
-        y = x.write(text)
-        return y
+        return x.write(text)
