2b62b258674c699e464d20ce0f4949504ad11b86
Angela-1610 <angelamaged2@gmail.com>
2023-11-07 14:15:13 -0800

    done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Defines a file-appending function."""
+
 
 def append_write(filename="", text=""):
     """function append text in file and return char no"""
-
     with open(filename, 'a', encoding='utf=8') as x:
         y = x.write(text)
         return y
